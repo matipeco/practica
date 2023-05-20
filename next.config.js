c6aad948d6255bee110d020a03e6a2f8688508d1
matipeco <matiaspecorale1995@gmail.com>
2023-05-19 23:50:46 -0300

    creando proyecto con next e instalando dependencias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    //https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
